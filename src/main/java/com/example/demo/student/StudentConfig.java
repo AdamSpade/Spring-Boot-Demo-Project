ff73ec9bec78710eeffa8fc0f030a66ff65a4d3f
Adam Spade <spade.adam46@gmail.com>
2022-02-24 11:17:58 -0800

    3rd Commit

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,41 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.AUGUST;
+import static java.time.Month.JUNE;
+
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            StudentRepository repository) {
+        return args -> {
+            Student dude = new Student(
+                    "Dude",
+                    "Dude.The@gmail.com",
+                    LocalDate.of(420, JUNE, 9),
+                    21
+            );
+
+            Student tony = new Student(
+                    "Tony",
+                    "Stark.Tony@gmail.com",
+                    LocalDate.of(1969, AUGUST, 1),
+                    54
+            );
+
+            repository.saveAll(
+                    List.of(dude, tony)
+            );
+        };
+    }
+}
